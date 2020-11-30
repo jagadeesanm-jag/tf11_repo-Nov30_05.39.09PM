c4dcbaf8d45f1046e2b4d0c942cc578b5db03619
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-11-30 17:39:41 +0530

    Modified files:[aws_two_tier.tf aws_two_tier.tf bad_file.tf bad_file.tf good.tf]

--- aws_two_tier_files_specified/good_file/good.tf
+++ aws_two_tier_files_specified/good_file/good.tf
@@ -316,3 +316,5 @@ output "FirewallManagementURL" {
 output "WebURL" {
   value = "${join("", list("http://", "${aws_eip.PublicElasticIP.public_ip}"))}"
 }
+
+# Modified [good.tf] on [2020-11-30 17:39:41.139079]
